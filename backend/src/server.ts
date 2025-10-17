ed71dc5fa127d8aedcc62ca117e2fa1d14c26658
Viktor Ralchenko <viktorr@finera.com>
2025-10-17 22:42:13 +0200

    Fixed buttons width.

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,6 +2,6 @@ import app from './app.js';
 
 const PORT = process.env.PORT ? parseInt(process.env.PORT, 10) : 3001;
 
-app.listen(PORT, () => {
-    console.log(`Backend server is running on http://localhost:${PORT}`);
+app.listen(PORT, '127.0.0.1', () => {
+    console.log(`Backend server is running on http://127.0.0.1:${PORT}`);
 });
